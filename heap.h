8f69f9d0ce93b337a68761644d0e42934622dd40
Krishna Katore <shrikrishnakatore@gmail.com>
2026-02-25 00:45:42 +0100

    Add header file for MAX heap implementation

---
+++ heap.h
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+// implementation of MAX heap
+typedef struct Heap{
+    int* data;
+    int capacity;
+    int size;
+} Heap;
+
+
+
+Heap *create_heap(int capacity);
+void delete_heap(Heap *h);
+void insert_node(Heap *h, int val);
+int extract_node(Heap *h);
+void printheap(Heap* h);
+
